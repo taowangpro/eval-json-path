5948d38edce31abdc0980e813af502d858669c97
taowangpro <tao.wang.pro@gmail.com>
2021-01-30 20:19:29 +0000

    fully jest tested

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-const getProp = (mem, prop) =>
-  (!mem || typeof mem !== 'object' || !Object.keys(mem).length) ? undefined : mem[prop];
+const getProp = (mem, prop) => (!mem || typeof mem !== 'object') ? undefined : mem[prop]
 
-const squareRE = /\[.+?\]/g;
+const squareRE = /\[.+?\]/g
 
 /**
-/*
  * support path.to.[very-pretty.name].more
- *
- * evalJsonPath({a:{b:1}, 'a.b':5}, 'a.b') ==> 1
- * evalJsonPath({a:{b:1}, 'a.b':5}, '[a.b]') ==> 5
  *
  * @param jsonObj The json obj to eval
  * @param path The path: step1.step2
  * @param defaultValue The fallback value
  * @returns The evaluate result
  */
-export default function evalJsonPath(jsonObj, path, defaultValue) {
-  if (!path) return jsonObj;
+function evalJsonPath (jsonObj, path, defaultValue) {
+  if (!path) return jsonObj
 
-  const camelProps = path.split(squareRE);
-  let result;
+  const camelProps = path.split(squareRE)
+  let result
 
   if (camelProps.length === 1) {
-    result = camelProps[0].split('.').reduce((mem, a) => getProp(mem, a), jsonObj);
+    result = camelProps[0].split('.').reduce((mem, a) => getProp(mem, a), jsonObj)
   } else {
-    const prettyProps = path.match(squareRE);
+    const prettyProps = path.match(squareRE)
     result = camelProps.reduce((mem, a, i) => {
-      mem = evalJsonPath(mem, a.replace(/^\.|\.$/g, ''));
+      mem = evalJsonPath(mem, a.replace(/^\.|\.$/g, ''))
       if (prettyProps[i]) {
-        mem = getProp(mem, prettyProps[i].replace(/^\[|\]$/g, ''));
+        mem = getProp(mem, prettyProps[i].replace(/^\[|\]$/g, ''))
       }
-      return mem;
-    }, jsonObj);
+      return mem
+    }, jsonObj)
   }
 
-  return result !== undefined ? result : defaultValue;
+  return result !== undefined ? result : defaultValue
 }
+
+module.exports = evalJsonPath
---
+++ index.spec.js
@@ -0,0 +1,47 @@
+const evalJsonPath = require('./index')
+
+describe('Date Translation', () => {
+  const obj = {
+    step1: {
+      step2: {
+        'pretty.nice-step': {
+          step3: 4
+        }
+      }
+    }
+  }
+
+  test('simple case', () => {
+    /* any not object, return undefined */
+    expect(evalJsonPath(null, 'step1')).toBe(undefined)
+
+    expect(evalJsonPath(undefined, 'step1')).toBe(undefined)
+
+    expect(evalJsonPath(3, 'step1')).toBe(undefined)
+
+    expect(evalJsonPath(false, 'step1')).toBe(undefined)
+
+    const result = evalJsonPath(obj, 'step1')
+    expect(result).toBe(obj.step1)
+  })
+
+  test('2 steps', () => {
+    const result = evalJsonPath(obj, 'step1.step2')
+    expect(result).toBe(obj.step1.step2)
+  })
+
+  test('nice-name steps', () => {
+    const result = evalJsonPath(obj, 'step1.step2[pretty.nice-step]')
+    expect(result).toBe(obj.step1.step2['pretty.nice-step'])
+  })
+
+  test('nice-name steps further', () => {
+    const result = evalJsonPath(obj, 'step1.step2[pretty.nice-step].step3')
+    expect(result).toBe(obj.step1.step2['pretty.nice-step'].step3)
+  })
+
+  test('any out of track', () => {
+    const result = evalJsonPath(obj, 'step1.XXX[pretty.nice-step].step3')
+    expect(result).toBe(undefined)
+  })
+})
